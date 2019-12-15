1156653fda9d070a789b49fb633ddbabfe41f1f0
Andrei Harbachou <andrei.harbachou@gmail.com>
2019-12-15 23:45:16 +0300

    Add comments in homework3_2

--- src/Homework3/Homework3_1.java
+++ src/Homework3/Homework3_1.java
@@ -1,5 +1,7 @@
 package Homework3;
-
+/*Написать класс в котором будут сожержаться все методы сортировок которые были заданы ранее.
+Начать их использовать в вашем коде.
+ */
 public class Homework3_1 {
     public static void main(String[] args) {
     }
