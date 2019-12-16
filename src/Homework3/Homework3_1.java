cebe12a477988dbc4f8dcbb1e44481b548389fd9
Andrei Harbachou <andrei.harbachou@gmail.com>
2019-12-16 15:53:55 +0300

    Added homework 3-3

--- src/Homework3/Homework3_1.java
+++ src/Homework3/Homework3_1.java
@@ -4,5 +4,22 @@ package Homework3;
  */
 public class Homework3_1 {
     public static void main(String[] args) {
+        //Сортировка выбором
+    }public static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int min = arr[i];
+            int min_i = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    min_i = j;
+                }
+            }
+            if (i != min_i) {
+                int tmp = arr[i];
+                arr[i] = arr[min_i];
+                arr[min_i] = tmp;
+            }
+        }
     }
 }
---
+++ src/Homework3/Homework3_3.java
@@ -0,0 +1,18 @@
+package Homework3;
+
+import java.util.Scanner;
+
+/*Сравнить две строки: "Мама мыла раму" и ввести эту же строку через консоль.
+При помощи оператора == и получить результаты сравнения true (да это возможно, как?)
+ */
+public class Homework3_3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = ("Мама мыла раму");
+        String str2 = in.nextLine();
+        System.out.println(str.intern() == str2.intern());
+    }
+}
+/* Использование метода intern гарантирует что введеная строка будет в пуле.
+Т.к. строки совпадают нам выдает значение true
+ */
