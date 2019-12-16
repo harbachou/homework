b4ed1f567b3b96e794ff6ca137f5a6f87b2e4ad0
Andrei Harbachou <andrei.harbachou@gmail.com>
2019-12-16 16:17:32 +0300

    Work on homework 3_1

--- src/Homework3/Homework3_1.java
+++ src/Homework3/Homework3_1.java
@@ -2,14 +2,17 @@ package Homework3;
 /*Написать класс в котором будут сожержаться все методы сортировок которые были заданы ранее.
 Начать их использовать в вашем коде.
  */
+//ПЕРВОЕ ЗАДАНИЕ В ПРОЦЕССЕ ВЫПОЛНЕНИЯ
 public class Homework3_1 {
     public static void main(String[] args) {
         //Сортировка выбором
-    }public static void selectionSort(int[] arr){
+    }
+
+    public static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int min = arr[i];
             int min_i = i;
-            for (int j = i+1; j < arr.length; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < min) {
                     min = arr[j];
                     min_i = j;
@@ -19,6 +22,19 @@ public class Homework3_1 {
                 int tmp = arr[i];
                 arr[i] = arr[min_i];
                 arr[min_i] = tmp;
+            }
+        }
+    }
+
+    //Сортировка пузырьком
+    public static void bubbleSort(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
             }
         }
     }
