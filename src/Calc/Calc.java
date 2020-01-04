216d0cc7ff7cc4d0de583e5a89284131e153cfa4
Andrei Harbachou <andrei.harbachou@gmail.com>
2020-01-04 15:27:20 +0300

    Modified calc

--- src/Calc/Calc.java
+++ src/Calc/Calc.java
@@ -1,28 +1,28 @@
 package Calc;
+    public class Calc {
 
-public class Calc {
+        public double calculate(double a, double b, String operator) {
 
-    public double calculate (double a, double b, String operator) {
-
-        double result = 0;
-        switch (operator){
-            case "+":
-                result = a+b;
-                break;
-            case "-":
-                result = a-b;
-                break;
-            case "*":
-                result = a*b;
-                break;
-            case "/":
-                result = a/b;
-                break;
-            default:
-                System.out.println("Неверный оператор");
+            double result = 0;
+            switch (operator) {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+                default:
+                    System.out.println("Неверный оператор");
+            }
+            return result;
         }
-        return result;
     }
-}
+
 
 
