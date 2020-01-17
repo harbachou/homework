4d08be085271f2f39ee8a031f84b21f8d1e21c78
Andrei Harbachou <andrei.harbachou@gmail.com>
2020-01-17 17:01:56 +0300

    Add homework 5_4

--- src/Homework5/Homework5_4.java
+++ src/Homework5/Homework5_4.java
@@ -1,4 +1,27 @@
 package Homework5;
+/*Полученный массив через консоль преобразовать в коллекцию и
+отсортировать данную коллекцию по возрастанию.
+ */
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Homework5_4 {
-}
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            int array[] = new int[5];
+            System.out.println("Введите числа ");
+            for (int i = 0; i < 5; i++) {
+                array[i] = input.nextInt();
+            }
+            System.out.print("Полученный массив: ");
+            for (int i = 0; i < 5; i++) {
+                System.out.print(" " + array[i]);
+            }
+            System.out.println("\nОтсортированный массив:");
+            Arrays.sort(array);
+            for (int i = 0; i< array.length; i++){
+                System.out.print(" " + array[i]);
+            }
+        }
+    }
+
